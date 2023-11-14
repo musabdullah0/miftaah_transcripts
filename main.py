7933f78d9e852c8614925807cc21c1c0f22425a5
Musab Abdullah <musababdullah6231@gmail.com>
2023-11-14 16:41:36 -0600

    using wkhtmltopdf and jinja2 now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from typing import List
+from enum import Enum
+from typing import List, Literal
 from fastapi import FastAPI, Form, Request, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
@@ -7,6 +8,10 @@ import uvicorn
 from pydantic import BaseModel
 from docx import Document
 from datetime import datetime
+import os
+from jinja2 import Template, Environment, FileSystemLoader
+
+
 
 class Course(BaseModel):
     name: str
@@ -21,13 +26,21 @@ class Period(BaseModel):
 
 class Student(BaseModel):
     name: str
+    program: Literal['Advanced', 'Associate', 'Online']
     grade: str
-    credits_earned: str
-    gpa: str
+    credits_earned: float
+    gpa: float
     expected_graduation: str
+
+def float_to_str(x: float) -> str:
+    return '{:.2f}'.format(round(x, 2))
 
 
 app = FastAPI()
+# templates = Jinja2Templates(directory="templates")
+env = Environment(loader=FileSystemLoader('templates'))
+
+
 
 @app.get("/")
 def read_item():
@@ -40,31 +53,18 @@ def read_item():
 
 @app.post("/transcript")
 async def generate_transcript(s: Student):
-    # update template file
-    doc_file_name = s.name.replace(" ", "") + ".docx"
-    doc = Document("Template_file.docx")
-    update = {
-        "{date_issued}": datetime.now().strftime("%B %d, %Y"),
-        "{name}": s.name,
-        "{grade}": s.grade,
-        "{credits}": s.credits_earned,
-        "{gpa}": s.gpa,
-        "{expected_grad}": s.expected_graduation
-    }
-    for p in doc.paragraphs:
-        for k, v in update.items():
-            if k in p.text:
-                print("before update: ", p.text)
-                p.text = p.text.replace(k, v)
-                print("after update: ", p.text)
-    
-    doc.save(doc_file_name)
-    pdf_file_name = doc_file_name.replace("docx", "pdf")
+    template = env.get_template('template.html')
+    html_content = template.render(**s.model_dump())
+    print(html_content[:100])
 
-    convert(doc_file_name, pdf_file_name, keep_active=True)
+    # html_content = templates.TemplateResponse("template.html", {"request": request, "name": s.name})
+    with open("input.html", "w+") as f:
+        f.write(html_content)
 
     # Return the generated PDF as a StreamingResponse
-    return StreamingResponse(open(pdf_file_name, "rb"), media_type="application/pdf", headers={"Content-Disposition": f"inline; filename={pdf_file_name}"})
+    os.system("wkhtmltopdf  --allow /Users/musab/Development/miftaah_transcripts/images input.html result.pdf")  
+    return StreamingResponse(open("result.pdf", "rb"), media_type="application/pdf", headers={"Content-Disposition": f"inline; filename=result.pdf"})
+
 
 
 if __name__ == "__main__":
